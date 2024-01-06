71d597696bd127b76ab01db4e4edee7fb3f39d4c
Kay_Kelvin <tiankaijunmao@163.com>
2024-01-06 22:40:59 +0800

    feat: 212
    2121

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends:[
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommend',
+        'plugin:prettier/recommended',
+        "prettier/@typescript-eslint"
+    ]
+}
